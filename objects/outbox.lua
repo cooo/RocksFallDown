164e40dae8842f27febcafe9f3f0e5978168a644
cooo <collin@Collins-MacBook-Pro.local>
2013-06-23 15:03:19 +0200

    fixed the 4-tiled outbox workaround

--- objects/outbox.lua
+++ objects/outbox.lua
@@ -1,30 +1,27 @@
 local outbox = boulderdash.Derive("base")
 outbox.hard = true
 outbox.flash_delay = 0.15
-outbox.strip = 4
+outbox.strip = 2
 
 -- the outbox looks like steel, until enough diamonds are collected,
 -- then the outbox flashes with the outbox bitmap
 function outbox:load( x, y )
 	local tileDeck = Moai:cachedTileDeck(boulderdash.imgpath .. "outbox.png", outbox.strip, 1)
 	outbox.prop  = Moai:createProp(layer, tileDeck, x, y)	
+end
 
-	local curve = MOAIAnimCurve.new ()
-	curve:reserveKeys ( outbox.strip )
-	for i=1, outbox.strip do
-		curve:setKey ( i, (i-1)*outbox.flash_delay, i, MOAIEaseType.FLAT )
+function outbox:flip_index()
+	if outbox.prop:getIndex()==1 then
+		outbox.prop:setIndex(2)
+	else
+		outbox.prop:setIndex(1)
 	end
-
-	outbox.anim = MOAIAnim:new ()
-	outbox.anim:reserveLinks ( 1 )
-	outbox.anim:setLink ( 1, curve, outbox.prop, MOAIProp2D.ATTR_INDEX )
-	outbox.anim:setMode ( MOAITimer.LOOP )
 end
 
 function outbox:consume()
 	if not outbox.hard then
 		boulderdash.setDone()
-		outbox.anim:stop()
+		outbox.flash_timer:stop()
 		outbox:remove()
 		return true
 	else
@@ -36,7 +33,10 @@ function outbox:update(dt)
 	-- animation of the outbox starts now	
 	if (boulderdash.diamonds >= level_loader.games[menu.game_index].caves[menu.cave_index].diamonds_to_get) then
 		outbox.hard = false -- so rockford can enter
-		outbox.anim:start ()
+
+		if not outbox.flash_timer then	-- start a timer only once
+			outbox.flash_timer = Moai:createLoopTimer(outbox.flash_delay, outbox.flip_index)
+		end
 	end
 end
 
